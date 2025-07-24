cd19f24f9554b0aba23fd9feee140517e83b3598
zenin5479 <zenin5479@gmail.com>
2025-07-24 20:09:18 +0300

    Update 3.2 Обновлен метод. Проведен рефакторинг

--- Program.cs
+++ Program.cs
@@ -15,6 +15,86 @@ namespace Homework_5._3
    {
       static void Main(string[] args)
       {
+         int[,] mas = new int[5, 5];
+         Random rnd = new Random();
+         // заполняем массив случайными числами от 0 до 9
+         for (int i = 0; i < 5; i++)
+         {
+
+            for (int j = 0; j < 5; j++)
+            {
+
+               mas[i, j] = rnd.Next(10);
+               Console.Write(mas[i, j] + "\t");
+            }
+            Console.WriteLine();
+         }
+         // определяем строку с минимальным  и максимальным элементом
+         int min = mas[1, 1];
+         int max = mas[1, 1];
+         int minstr = 0, maxstr = 0;
+         // определяем минимальную строку
+         for (int i = 1; i < 5; i++)
+         {
+            for (int j = 0; j < 5; j++)
+            {
+               if (mas[i, j] < min)
+               {
+                  min = mas[i, j];
+                  minstr = i;
+               }
+
+            }
+         }
+         // определяем максимальную строку
+         for (int i = 1; i < 5; i++)
+         {
+            for (int j = 0; j < 5; j++)
+            {
+               if (mas[i, j] > max)
+               {
+                  max = mas[i, j];
+                  maxstr = i;
+               }
+            }
+         }
+
+         Console.WriteLine("Меняем местами строки {0} и {1} строку", minstr + 1, maxstr + 1);
+         // перестановка строк
+         int[] tempmas = new int[5];
+         //Заносим минимальную строку во временный массив
+         for (int i = minstr, j = 0, k = 0; j < 5; j++, k++)
+         {
+
+            tempmas[k] = mas[i, j];
+
+         }
+         //Заменяем минимальную строку максимальной
+         for (int j = 0; j < 5; j++)
+         {
+            mas[minstr, j] = mas[maxstr, j];
+         }
+         //Заменяем максимальную строку минимальной
+         for (int j = 0; j < 5; j++)
+         {
+            mas[maxstr, j] = tempmas[j];
+         }
+
+         // вывод измененного массива
+         Console.WriteLine("------------------------");
+         for (int i = 0; i < 5; i++)
+         {
+            for (int j = 0; j < 5; j++)
+            {
+               Console.Write(mas[i, j] + "\t");
+            }
+            Console.WriteLine();
+         }
+         Console.ReadKey();
+
+
+
+
          int[] array = { -93, -84, 84, 95, 93, 71, -36, -8, 37, -11, -3, -73, -31, -27, -14, -55, 1, 45, 81, 47 };
          int[] expected = { 55, -23, 35, -56, 90, 59, 72, -53, 63, 68, -71, 87, -9, -63, 24, -88, 24, 80, 41, 30 };
 
@@ -49,6 +129,25 @@ namespace Homework_5._3
          //}
 
          //Console.WriteLine();
+      }
+
+      private void SwitchRows(int[,] array)
+      {
+         var iMax = array.GetLength(0);
+         var jMax = array.GetLength(1);
+
+         for (int i = 0; i < iMax; i = i + 2)
+         {
+            if (i + 1 == iMax)
+               break;
+
+            for (int j = 0; j < jMax; j++)
+            {
+               var a = array[i, j];
+               array[i, j] = array[i + 1, j];
+               array[i + 1, j] = a;
+            }
+         }
       }
 
       void ChangeArray(int[,] array)
