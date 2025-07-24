caf7fead8a6d5f63ee4a02d37c2f91bb599ab423
zenin5479 <zenin5479@gmail.com>
2025-07-24 20:03:54 +0300

    Update 3.1 Обновлен метод. Проведен рефакторинг

--- Program.cs
+++ Program.cs
@@ -51,6 +51,28 @@ namespace Homework_5._3
          //Console.WriteLine();
       }
 
+      void ChangeArray(int[,] array)
+      {
+         for (int i = 0; i < array.GetLength(0); i++)
+         {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+               if (i > 0 && i < array.GetLength(1))
+               {
+                  int temporary = array[i, j];
+                  array[i, j] = array[i, j];
+                  array[i, j] = temporary;
+               }
+               else
+               {
+                  int temporary = array[i, j];
+                  array[i, j] = array[(array.GetLength(1) - 1), j];
+                  array[(array.GetLength(1) - 1), j] = temporary;
+               }
+            }
+         }
+      }
+
       public static int[] SortArray(int[] numArray)
       {
          int x = 0;
