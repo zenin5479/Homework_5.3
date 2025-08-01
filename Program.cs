8bda0a27cf56c4ca3c49f48de5f3eead9e799635
zenin5479 <zenin5479@gmail.com>
2025-08-01 08:37:28 +0300

    Update 19.9 Обновлен метод. Проведен рефакторинг

--- Program.cs
+++ Program.cs
@@ -33,7 +33,7 @@ namespace Homework_5._3
             EnterArrayDouble(sumRow);
             Console.WriteLine();
 
-            double[,] sortArray = BubbleSort(inputArray, sumRow); // Вернуть двумерный массив double[,] 
+            double[,] sortArray = BubbleSort(inputArray, sumRow);
             string pathFileInput = Path.GetFullPath(nameFileInput);
             File.Create(pathFileInput).Close();
             // Написать метод OutputStringDouble для двумерного массива double[,]
