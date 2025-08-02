1e543e6550ac90f32b995833823f00c94b3833ea
zenin5479 <zenin5479@gmail.com>
2025-08-02 09:04:26 +0300

    Update 22.3 Обновлен метод. Проведен рефакторинг

--- Program.cs
+++ Program.cs
@@ -83,26 +83,19 @@ namespace Homework_5._3
             int m = 0;
             while (m < array.GetLength(1))
             {
-               if (column == inputArray.GetLength(1) - 1)
+               if (m == array.GetLength(1) - 1)
                {
-                  stringModified.Append(inputArray[row, column]);
-                  subLine = stringModified.ToString();
-                  arrayString[row] = subLine;
+                  Console.Write(array[l, m]);
+                  Console.Write("{0:f}", array[l, m]);
+                  Console.Write("{0:f2}", array[l, m]);
                }
                else
                {
-                  stringModified.Append(inputArray[row, column] + " ");
-                  subLine = stringModified.ToString();
-                  arrayString[row] = subLine;
+                  Console.Write(array[l, m] + " ");
+                  //Console.Write("{0:f} ", array[l, m]);
+                  //Console.Write("{0:f2} ", array[l, m]);
                }
 
-
-
-
-
-               Console.Write(array[l, m] + " ");
-               //Console.Write("{0:f} ", array[l, m]);
-               //Console.Write("{0:f2} ", array[l, m]);
                m++;
             }
 
